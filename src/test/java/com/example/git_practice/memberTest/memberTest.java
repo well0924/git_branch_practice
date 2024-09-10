0a18030f47cc9bc59a4a84fdfacf0c7f8197ac52
well0924 <well414965@gmail.com>
2024-09-11 02:25:19 +0900

    test 회원 테스트에 대한 클래스 작성

---
+++ src/test/java/com/example/git_practice/memberTest/memberTest.java
@@ -0,0 +1,15 @@
+package com.example.git_practice.memberTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class memberTest {
+
+    @Test
+    @DisplayName("회원 목록")
+    public void memberList(){
+
+    }
+}
